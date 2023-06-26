21550734c693d06f265759b5491deeced0426693
Muhammad <tazakka.ramadhan@delameta.com>
2023-06-26 14:10:41 +0700

    add value component

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-type ValueProps = {
-  label: string;
-  value: string;
-};
-
-const Value = ({ label, value }: ValueProps) => (
-  <View>
-    <Text style={styles.label}>{label}</Text>
-    <Text style={styles.value}>{value}</Text>
-  </View>
-);
+import Value from './src/components/Value';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style='auto' />
+
       <View style={styles.values}>
         <Value label='Steps' value='1219' />
         <Value label='Distance' value='0,75 km' />
         <Value label='Flights Climb' value='12' />
       </View>
-      <StatusBar style='auto' />
+
     </View>
   );
 }
@@ -34,13 +25,4 @@ const styles = StyleSheet.create({
     padding: 12,
   },
   values: { flexDirection: 'row', gap:25, flexWrap: 'wrap' },
-  label: {
-    color: 'white',
-    fontSize: 20,
-  },
-  value: {
-    fontSize: 45,
-    color: '#AFB3BE',
-    fontWeight: '500',
-  },
 });
