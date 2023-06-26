7b2bbb705eada4f7275b8b202362d3d53d02588f
Muhammad <tazakka.ramadhan@delameta.com>
2023-06-26 13:59:25 +0700

    add static text;

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type ValueProps = {
+  label: string;
+  value: string;
+};
+
+const Value = ({ label, value }: ValueProps) => (
+  <View>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.values}>
+        <Value label='Steps' value='1219' />
+        <Value label='Distance' value='0,75 km' />
+        <Value label='Flights Climb' value='12' />
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +29,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    padding: 12,
+  },
+  values: { flexDirection: 'row', gap:25, flexWrap: 'wrap' },
+  label: {
+    color: 'white',
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 45,
+    color: '#AFB3BE',
+    fontWeight: '500',
   },
 });
