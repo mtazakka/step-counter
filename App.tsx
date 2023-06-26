b69acf24608e9ceb23506fe05eae6ee9dced2bf0
Muhammad <tazakka.ramadhan@delameta.com>
2023-06-26 14:49:14 +0700

    Progress Ring

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import RingProgress from './src/components/RingProgress';
 import Value from './src/components/Value';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
+      <RingProgress progress={0.30} />
 
       <View style={styles.values}>
         <Value label='Steps' value='1219' />
         <Value label='Distance' value='0,75 km' />
         <Value label='Flights Climb' value='12' />
       </View>
-
     </View>
   );
 }
@@ -24,5 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 12,
   },
-  values: { flexDirection: 'row', gap:25, flexWrap: 'wrap' },
+  values: { flexDirection: 'row', gap: 25, flexWrap: 'wrap', marginTop: 50 },
 });
--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -1,52 +1,17 @@
-import { View } from 'react-native';
-import SVG, { Circle, CircleProps } from 'react-native-svg';
-import Animated, {
-  useAnimatedProps,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
-import { useEffect } from 'react';
-import { AntDesign } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Circle, Svg } from 'react-native-svg';
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-
-type RingProgressProps = {
+type Props = {
   radius?: number;
   strokeWidth?: number;
-  progress: number;
+  progress?: number;
 };
-
 const color = '#EE0F55';
 
-const RingProgress = ({
-  radius = 100,
-  strokeWidth = 35,
-  progress,
-}: RingProgressProps) => {
+const RingProgress = ({ radius = 100, strokeWidth = 35, progress }: Props) => {
   const innerRadius = radius - strokeWidth / 2;
   const circumference = 2 * Math.PI * innerRadius;
-
-  const fill = useSharedValue(0);
-
-  useEffect(() => {
-    fill.value = withTiming(progress, { duration: 1500 });
-  }, [progress]);
-
-  const animatedProps = useAnimatedProps(() => ({
-    strokeDasharray: [circumference * fill.value, circumference],
-  }));
-
-  const circleDefaultProps: CircleProps = {
-    r: innerRadius,
-    cx: radius,
-    cy: radius,
-    originX: radius,
-    originY: radius,
-    strokeWidth: strokeWidth,
-    stroke: color,
-    strokeLinecap: 'round',
-    rotation: '-90',
-  };
 
   return (
     <View
@@ -56,24 +21,38 @@ const RingProgress = ({
         alignSelf: 'center',
       }}
     >
-      <SVG>
+      <Svg>
         {/* Background */}
-        <Circle {...circleDefaultProps} opacity={0.2} />
+        <Circle
+          cx={radius}
+          cy={radius}
+          r={innerRadius}
+          // fill={'black'}
+          stroke={color}
+          strokeWidth={strokeWidth}
+          opacity={0.2}
+          // rotation='-90'
+        />
+
         {/* Foreground */}
-        <AnimatedCircle animatedProps={animatedProps} {...circleDefaultProps} />
-      </SVG>
-      <AntDesign
-        name="arrowright"
-        size={strokeWidth * 0.8}
-        color="black"
-        style={{
-          position: 'absolute',
-          alignSelf: 'center',
-          top: strokeWidth * 0.1,
-        }}
-      />
+        <Circle
+          r={innerRadius}
+          cx={radius}
+          cy={radius}
+          originX={radius}
+          originY={radius}
+          // fill={'black'}
+          stroke={color}
+          strokeWidth={35}
+          strokeDasharray={[circumference * progress, circumference]}
+          strokeLinecap='round'
+          rotation='-90'
+        />
+      </Svg>
     </View>
   );
 };
 
 export default RingProgress;
+
+const styles = StyleSheet.create({});
