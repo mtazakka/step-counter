f71610cc95e09304a2f01aab6ea2f7020ab2fcfb
Muhammad <tazakka.ramadhan@delameta.com>
2023-06-27 15:10:39 +0700

    [ADD] react native health for ios and android

--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -1,27 +1,28 @@
-import { AntDesign } from '@expo/vector-icons';
-import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
+import SVG, { Circle, CircleProps } from 'react-native-svg';
 import Animated, {
   useAnimatedProps,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
-import { Circle, CircleProps, Svg } from 'react-native-svg';
+import { useEffect } from 'react';
+import { AntDesign } from '@expo/vector-icons';
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
-type Props = {
+type RingProgressProps = {
   radius?: number;
   strokeWidth?: number;
-  progress?: number;
+  progress: number;
 };
+
 const color = '#EE0F55';
 
 const RingProgress = ({
   radius = 100,
   strokeWidth = 35,
-  progress = 1,
-}: Props) => {
+  progress,
+}: RingProgressProps) => {
   const innerRadius = radius - strokeWidth / 2;
   const circumference = 2 * Math.PI * innerRadius;
 
@@ -41,8 +42,8 @@ const RingProgress = ({
     cy: radius,
     originX: radius,
     originY: radius,
-    stroke: color,
     strokeWidth: strokeWidth,
+    stroke: color,
     strokeLinecap: 'round',
     rotation: '-90',
   };
@@ -55,23 +56,24 @@ const RingProgress = ({
         alignSelf: 'center',
       }}
     >
-      <Svg>
+      <SVG>
         {/* Background */}
-        <Circle opacity={0.2} {...circleDefaultProps} />
-
+        <Circle {...circleDefaultProps} opacity={0.2} />
         {/* Foreground */}
         <AnimatedCircle animatedProps={animatedProps} {...circleDefaultProps} />
-      </Svg>
+      </SVG>
       <AntDesign
-        name='arrowright'
+        name="arrowright"
         size={strokeWidth * 0.8}
-        color='black'
-        style={{ position: 'absolute', alignSelf: 'center', top: strokeWidth * 0.1 }}
+        color="black"
+        style={{
+          position: 'absolute',
+          alignSelf: 'center',
+          top: strokeWidth * 0.1,
+        }}
       />
     </View>
   );
 };
 
 export default RingProgress;
-
-const styles = StyleSheet.create({});
